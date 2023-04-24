488f59614ce404afbd06564f67ce357129a3285f
andreis3 <andrei.as3@hotmail.com>
2023-04-24 08:39:37 -0300

    create producer msg end

---
+++ cmd/producer/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/andreis3/fcutils/pkg/rabbitmq"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+
+	defer ch.Close()
+
+	rabbitmq.Publish(ch, "Hello World!", "amq.direct")
+}
